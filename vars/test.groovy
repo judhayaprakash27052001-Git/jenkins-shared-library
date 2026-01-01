9bfd2048ac57ced1c5e1dafb0a03067f57ad25f4
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 15:04:23 +0530

    Delete vars directory

--- vars/test.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config) {
-    sh """
-        docker rm -f ${config.testContainer} || true
-        docker run -d --name ${config.testContainer} -p ${config.testPort}:80 ${config.image}:${config.tag}
-        sleep 5
-        docker rm -f ${config.testContainer}
-    """
-}
