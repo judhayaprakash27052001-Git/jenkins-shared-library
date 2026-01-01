2207ff353027ea7cf9e31a06bae5116399569418
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 10:13:13 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,4 +1,8 @@
-def call(String env) {
-    echo "Shared Library: Deploying to ${env}"
-    bat "echo Deploying to ${env} from shared library"
+def call(String appName, String env) {
+    echo "Deploying ${appName} to ${env}"
+
+    bat """
+    docker rm -f ${appName}-prod || echo No running container
+    docker run -d --name ${appName}-prod -p 8080:80 ${appName}:latest
+    """
 }
