084eb0cba45b8a28f595ac11a058d244c799faa3
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 10:12:50 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,10 @@
-def call() {
-    echo "Shared Library: Running tests"
-    bat "echo Tests executed from shared library"
+def call(String appName) {
+    echo "Testing Docker container for ${appName}"
+
+    bat """
+    docker run -d --name ${appName}-test -p 8085:80 ${appName}:latest
+    timeout /t 5
+    docker ps | findstr ${appName}-test
+    docker rm -f ${appName}-test
+    """
 }
