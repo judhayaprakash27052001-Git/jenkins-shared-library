6caff336a1ec43ef892318bb51ab19bc746ecbc7
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 14:06:55 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,16 +1,18 @@
-def call(String appName) {
-    echo "Building Docker image for ${appName}"
+def call() {
+    def imageTag = env.GIT_COMMIT
 
-    withCredentials([usernamePassword(
-        credentialsId: 'dockerhub-creds',
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
-        bat """
-        echo %DOCKER_PASS% | docker login -u %DOCKER_USER% --password-stdin
-        docker build --no-cache -t %DOCKER_USER%/${appName}:latest .
-        docker logout
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker-hub-creds',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+        sh """
+          echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+          docker build --no-cache -t udhaya2705/portfolio-app:${imageTag} .
+          docker push udhaya2705/portfolio-app:${imageTag}
+          docker logout
         """
     }
 }
-
