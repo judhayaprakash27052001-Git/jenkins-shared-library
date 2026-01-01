e5bb7c3d0ac59ed407a225e07fa39a61e5ed510c
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 14:07:43 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,19 @@
-def call(String appName, String env) {
-    withCredentials([usernamePassword(
-        credentialsId: 'dockerhub-creds',
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
-        bat """
-        echo %DOCKER_PASS% | docker login -u %DOCKER_USER% --password-stdin
-        docker rm -f ${appName}-prod || echo No running container
-        docker run -d --name ${appName}-prod -p 8080:80 %DOCKER_USER%/${appName}:latest
-        docker logout
+def call() {
+    def imageTag = env.GIT_COMMIT
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker-hub-creds',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+        sh """
+          echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+          docker rm -f portfolio-app-prod || true
+          docker pull udhaya2705/portfolio-app:${imageTag}
+          docker run -d --name portfolio-app-prod -p 8080:80 udhaya2705/portfolio-app:${imageTag}
+          docker logout
         """
     }
 }
