825a792146eb3ec4b953980c900828ec8532452a
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 11:04:56 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,14 @@
 def call(String appName, String env) {
-    echo "Deploying ${appName} to ${env}"
-
-    bat """
-    docker rm -f ${appName}-prod || echo No running container
-    docker run -d --name ${appName}-prod -p 8080:80 ${appName}:latest
-    """
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        bat """
+        echo %DOCKER_PASS% | docker login -u %DOCKER_USER% --password-stdin
+        docker rm -f ${appName}-prod || echo No running container
+        docker run -d --name ${appName}-prod -p 8080:80 %DOCKER_USER%/${appName}:latest
+        docker logout
+        """
+    }
 }
