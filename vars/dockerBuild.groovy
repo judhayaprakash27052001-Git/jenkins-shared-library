89528a7b6277ea14955b8ffe15301df07ea0acc0
UDHAYAPRAKASH JYAVEL <judhayaprakash27052001@gmail.com>
2026-01-01 15:04:40 +0530

    Create dockerBuild.groovy

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,16 @@
+def call(Map config) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: config.dockerCreds,
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+        bat """
+        echo %DOCKER_PASS% | docker login -u %DOCKER_USER% --password-stdin
+        docker build --no-cache -t ${config.image}:${config.tag} .
+        docker push ${config.image}:${config.tag}
+        docker logout
+        """
+    }
+}
